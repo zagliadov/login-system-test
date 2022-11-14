8925eb72f953c34ee1f25fbf74a7fa70ac781f01
Daniil Zahliadov <portal.sid.grand2@gmail.com>
2022-11-14 11:03:26 +0200

    Upgrade the useLocalStorage Hook

--- client/src/features/hooks/useLocalStorage.js
+++ client/src/features/hooks/useLocalStorage.js
@@ -1,16 +1,30 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
-export function useLocalStorage(key, initialValue) {
-  const [value, setValue] = useState(() => {
-    const item = window.localStorage.getItem(key);
-    return item ? JSON.parse(item) : initialValue;
+export const useLocalStorage = (key, initialValue) => {
+  const [storedValue, setStoredValue] = useState(() => {
+    if (typeof window === "undefined") {
+      return initialValue;
+    }
+    try {
+      const item = window.localStorage.getItem(key);
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      console.log(error);
+      return initialValue;
+    }
   });
 
-  useEffect(() => {
-    const item = JSON.stringify(value);
-    window.localStorage.setItem(key, item);
-    // eslint-disable-next-line
-  }, [value]);
-
-  return [value, setValue];
-}
+  const setValue = (value) => {
+    try {
+      const valueToStore =
+        value instanceof Function ? value(storedValue) : value;
+      setStoredValue(valueToStore);
+      if (typeof window !== "undefined") {
+        window.localStorage.setItem(key, JSON.stringify(valueToStore));
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return [storedValue, setValue];
+};

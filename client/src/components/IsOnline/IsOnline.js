d3400448b548560bb2248800f55ff405b20452e1
Daniil Zahliadov <portal.sid.grand2@gmail.com>
2022-11-14 14:25:54 +0200

    Representing the IsOnline Hook

---
+++ client/src/components/IsOnline/IsOnline.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { useOnline } from "../../features/hooks/useOnline";
+
+export const IsOnline = () => {
+  const status = useOnline();
+  return (
+    <p
+      className={`animate-pulse ${
+        status ? "bg-lime-400" : "bg-red-500"
+      } border w-2 h-2 rounded-full`}
+    ></p>
+  );
+};

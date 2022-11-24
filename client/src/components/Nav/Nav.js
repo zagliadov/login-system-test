2fcaacc91dcc4ce39878315bcc7919c02a9fac38
Daniil Zahliadov <portal.sid.grand2@gmail.com>
2022-11-24 11:56:06 +0200

    create TableStudents component

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/alt-text */
 import React from "react";
 import { IsOnline } from "../IsOnline/IsOnline";
 import { CustomLink } from "../CustomLink/CustomLink";
@@ -9,18 +10,16 @@ export const Nav = () => {
         <IsOnline />
       </div>
       <div className="flex items-center">
-        <CustomLink to="/">
-          Home
-        </CustomLink>
-        <CustomLink to="/private">
-          Private
-        </CustomLink>
-        <CustomLink to="/signin">
-          Sign In
-        </CustomLink>
-        <CustomLink to="/signup">
-          Sign Up
-        </CustomLink>
+        <CustomLink to="/">Home</CustomLink>
+        <CustomLink to="/private">Private</CustomLink>
+        <CustomLink to="/signin">Sign In</CustomLink>
+        <CustomLink to="/signup">Sign Up</CustomLink>
+        <div className="shrink-0">
+          <img
+            className="h-12 w-12 object-cover rounded-full"
+            src="https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1361&q=80"
+          />
+        </div>
       </div>
     </header>
   );
--- client/src/components/Private/Private.js
+++ client/src/components/Private/Private.js
@@ -1,23 +1,11 @@
 import React from "react";
-import { Link, useOutletContext } from "react-router-dom";
+import { TableStudents } from "./TableStudents/TableStudents";
 
 export const Private = () => {
-  const [isLoggedIn, user] = useOutletContext();
 
   return (
-    <div className="flex flex-col border items-center pt-5 text-xl">
-      <div className="pt-8">hello {user?.email}</div>
-      <div>
-        <svg
-          className="h-6 w-6 flex-none fill-sky-100 stroke-sky-500 stroke-2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        >
-          <circle cx="12" cy="12" r="11" />
-          <path d="m8 13 2.165 2.165a1 1 0 0 0 1.521-.126L16 9" fill="none" />
-        </svg>
-        <Link to="/private/students">students</Link>
-      </div>
+    <div className="flex flex-col text-xl">
+      <TableStudents />
     </div>
   );
 };
